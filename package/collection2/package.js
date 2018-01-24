9f4df59d0746b934e80717544eba5f525e5a4089
Eric Dobbertin <eric@dairystatedesigns.com>
2018-01-24 08:38:55 -0600

    Publish 2.1.2

--- package/collection2/package.js
+++ package/collection2/package.js
@@ -3,7 +3,7 @@
 Package.describe({
   name: "aldeed:collection2-core",
   summary: "Core package for aldeed:collection2",
-  version: "2.1.1",
+  version: "2.1.2",
   documentation: "../../README.md",
   git: "https://github.com/aldeed/meteor-collection2-core.git"
 });
